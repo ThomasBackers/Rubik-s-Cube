528362cd6145919cd4ea876624b7992cfb3a5524
ThomasBackers <thomasbackers@gmail.com>
2021-11-02 16:10:12 +0100

    brainfart on JS but satisfying result :D let's optimize it

--- src/js/main.js
+++ src/js/main.js
@@ -27,4 +27,360 @@ for (let cube of cubes) {
 // top center front, top center between, top center back,
 // top right front, top right between, top right back,
 // center left front, center left between, center left back,
-// etc 
+// etc
+
+// Let's hide useless sides to avoid display bugs
+// and assign a color to the others
+
+for (let cubeIndex = 0; cubeIndex < 27; cubeIndex++) {
+    switch(cubeIndex) {
+        case 0:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 1:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 2:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 3:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 4:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 5:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 6:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 7:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 8:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 4:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "green"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 9:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 10:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 11:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 12:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 13:
+            for(let side of cubesSides[cubeIndex]) 
+                side.style.display = "none"
+            break
+        case 14:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 15:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 16:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 17:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 18:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 19:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 20:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 2:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "blue";
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 21:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 22:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 23:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 24:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 0:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "red";
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 25:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+        case 26:
+            for (let sideIndex = 0; sideIndex < 6; sideIndex++)
+                switch (sideIndex) {
+                    case 1:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "yellow"
+                        break
+                    case 3:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "white"
+                        break
+                    case 5:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "orange"
+                        break
+                    default:
+                        cubesSides[cubeIndex][sideIndex].style.backgroundColor = "black"
+                }
+            break
+    }
+}   
